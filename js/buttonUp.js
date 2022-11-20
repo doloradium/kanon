1a3fe7cce57875926a9321ad8c206bfb71efd3ba
McColum <valerymakkol@gmail.com>
2022-11-20 18:16:34 +0300

    v 0.0.5.3

--- js/buttonUp.js
+++ js/buttonUp.js
@@ -1,12 +1,19 @@
-(function() {
-    $(window).scroll(function() {
-        if($(this).scrollTop() != 0) {
-            $('.button__top').fadeIn();
-        } else {
-            $('.button__top').fadeOut();
-        }
-    });
-    $('.button__top').click(function() {
-        $('body, html').animate({scrollTop: 0}, 0);
-    });
-});
+let box = document.getElementById('buttonUp')
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        box.classList.remove('button__top-hidden');
+        setTimeout(function () {
+            box.classList.remove('button__top-opacity');
+        }, 20);
+    } else {
+        box.classList.add('button__top-opacity');    
+        box.addEventListener('transitionend', function(e) {
+        }, {
+            capture: false,
+            once: true,
+            passive: false
+        });
+    }
+}
