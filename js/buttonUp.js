60168b1be6e43b81829abc8716a5c9b930c55513
McColum <valerymakkol@gmail.com>
2022-11-20 17:01:55 +0300

    v 0.0.5.1

---
+++ js/buttonUp.js
@@ -0,0 +1,12 @@
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('.button__top').fadeIn();
+        } else {
+            $('.button__top').fadeOut();
+        }
+    });
+    $('.button__top').click(function() {
+        $('body, html').animate({scrollTop: 0}, 0);
+    });
+});
