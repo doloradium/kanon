e18a3c3d71824b78865b475a05efdd0e81e42952
McColum <valerymakkol@gmail.com>
2022-11-22 23:47:35 +0300

    v 0.0.6

--- js/buttonUp.js
+++ js/buttonUp.js
@@ -1,19 +1,12 @@
-let box = document.getElementById('buttonUp')
-window.onscroll = function() {scrollFunction()};
+let buttonUp = document.getElementById('buttonUp')
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        box.classList.remove('button__top-hidden');
-        setTimeout(function () {
-            box.classList.remove('button__top-opacity');
-        }, 20);
+window.onscroll = () => {
+    if (document.documentElement.scrollTop > 20) {
+        buttonUp.classList.remove('hidden')
+        setTimeout(function() {
+            buttonUp.classList.remove('opacity-0')
+        }, 10)
     } else {
-        box.classList.add('button__top-opacity');    
-        box.addEventListener('transitionend', function(e) {
-        }, {
-            capture: false,
-            once: true,
-            passive: false
-        });
+        buttonUp.classList.add('opacity-0')
     }
 }
